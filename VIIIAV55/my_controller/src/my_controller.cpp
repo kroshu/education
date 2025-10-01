807d050d1c24c878c57c0c9472cf1f8f16cb9fa4
Kristof Pasztor <pasztor.kristof.kp@gmail.com>
2025-10-01 09:43:27 +0200

    Replace my_controller implementation with TODO

--- VIIIAV55/my_controller/src/my_controller.cpp
+++ VIIIAV55/my_controller/src/my_controller.cpp
@@ -24,55 +24,54 @@ namespace my_controller
 
     controller_interface::InterfaceConfiguration MyController::command_interface_configuration() const
     {
-        return get_interface_configuration();
+        // TODO 3
+        // Specify the command interfaces that this controller will write to.
+        // You can run `ros2 control list_hardware_interfaces` in your terminal to view all available interfaces.
+
+        return {};
     }
 
     controller_interface::InterfaceConfiguration MyController::state_interface_configuration() const
     {
-        return get_interface_configuration();
+        // TODO 4
+        // Specify the state interfaces that this controller will read from.
+        // You can run `ros2 control list_hardware_interfaces` in your terminal to view all available interfaces.
+
+        return {};
     }
 
     controller_interface::CallbackReturn MyController::on_configure(const rclcpp_lifecycle::State &)
     {
-        max_velocity_ = get_node()->get_parameter("max_velocity").as_double();
-        kp_ = get_node()->get_parameter("kp").as_double_array();
-        num_joints_ = kp_.size();
-        target_cache_.assign(num_joints_, 0.0);
-        cmd_sub_ = get_node()->create_subscription<std_msgs::msg::Float64MultiArray>(
-            "~/command", rclcpp::SystemDefaultsQoS(),
-            [this](const std_msgs::msg::Float64MultiArray::SharedPtr msg)
-            {
-                rt_target_pos_.writeFromNonRT(msg->data);
-            });
+        // TODO 1
+        // Read controller parameters (i.e. max_velocity, kp) from node parameters and initialize related variables.
+        // max_velocity_ = ...
+        // kp_ = ...
+        // num_joints_ = ...
+        // target_cache_ = ...
+
+        // TODO 2
+        // Create a subscription to receive target joint positions and update rt_target_pos_ accordingly.
+
         return controller_interface::CallbackReturn::SUCCESS;
     }
 
     controller_interface::CallbackReturn MyController::on_activate(const rclcpp_lifecycle::State &)
     {
-        std::vector<double> current(num_joints_, 0.0);
-        for (size_t i = 0; i < num_joints_; i++)
-        {
-            current[i] = state_interfaces_[i].get_value();
-            command_interfaces_[i].set_value(current[i]);
-        }
-        rt_target_pos_.initRT(current);
+        // TODO 5
+        // Initialize the real-time target position buffer with the current joint positions.
+        // Also, set the command interfaces to the current joint positions to avoid jumps.
 
         return controller_interface::CallbackReturn::SUCCESS;
     }
 
     controller_interface::return_type MyController::update(const rclcpp::Time &, const rclcpp::Duration &period)
     {
-        // Get latest target
-        std::copy_n(rt_target_pos_.readFromRT()->data(), num_joints_, target_cache_.data());
+        // TODO 6
+        // Get latest target from the real-time buffer
 
-        // Interpolate joint positions
-        const double max_step = max_velocity_ * period.seconds();
-        for (size_t i = 0; i < num_joints_; i++)
-        {
-            const double error = target_cache_[i] - state_interfaces_[i].get_value();
-            const double u = std::clamp(kp_[i] * error, -max_step, +max_step);
-            command_interfaces_[i].set_value(state_interfaces_[i].get_value() + u);
-        }
+        // TODO 7
+        // For each joint, update its command interface so that the joint moves towards its target position.
+        // Make sure that the velocity for each joint does not exceed max_velocity_.
 
         return controller_interface::return_type::OK;
     }
