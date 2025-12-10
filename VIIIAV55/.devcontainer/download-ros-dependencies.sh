f23f56c62c11fa5326836b7fb3631e808ea8469e
Kristof Pasztor <pasztor.kristof.kp@gmail.com>
2025-12-10 14:09:22 +0100

    Update Dockerfile to include rqt

--- VIIIAV55/.devcontainer/download-ros-dependencies.sh
+++ VIIIAV55/.devcontainer/download-ros-dependencies.sh
@@ -3,5 +3,8 @@ set -euo pipefail
 
 rosdep update
 sudo apt-get update
-sudo apt upgrade -y
+sudo apt-get upgrade -y
 rosdep install --from-paths . --ignore-src --rosdistro humble -y
+
+# Install further ROS-related tools
+sudo apt-get install ros-humble-rqt* -y
