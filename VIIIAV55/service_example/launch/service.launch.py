8f321bb8a9084b7ef3d32c6d8e91c15f2379ee4f
KissAnnaErika <kiss.anna203@gmail.com>
2026-09-08 16:08:53 +0200

    Added launch file

---
+++ VIIIAV55/service_example/launch/service.launch.py
@@ -0,0 +1,34 @@
+from launch import LaunchDescription
+from launch_ros.actions import Node
+from launch.actions import DeclareLaunchArgument
+from launch.substitutions import LaunchConfiguration
+
+
+
+def generate_launch_description():
+    declare_birth_date = DeclareLaunchArgument(
+        'birth_date',
+        default_value='2003.03.02.',
+        description='Birth date to send to the get_age service'
+    )
+
+    server = Node(
+            package='service_example',
+            executable='server',
+            name='server'
+        )
+
+    client = Node(
+                package='service_example',
+                executable='client',
+                name='client',
+                parameters=[{
+                    'birth_date': LaunchConfiguration('birth_date')
+                }]
+            )
+
+    return LaunchDescription([
+        declare_birth_date,
+        server,
+        client,
+    ])
--- VIIIAV55/service_example/setup.py
+++ VIIIAV55/service_example/setup.py
@@ -1,4 +1,6 @@
 from setuptools import find_packages, setup
+import os
+from glob import glob
 
 package_name = 'service_example'
 
@@ -10,6 +12,7 @@ setup(
         ('share/ament_index/resource_index/packages',
             ['resource/' + package_name]),
         ('share/' + package_name, ['package.xml']),
+        (os.path.join('share', package_name, 'launch'), glob(os.path.join('launch', '*launch.py'))),
     ],
     install_requires=['setuptools'],
     zip_safe=True,
@@ -24,7 +27,7 @@ setup(
     },
     entry_points={
         'console_scripts': [
-            'service = service_example.get_age_server:main',
+            'server = service_example.get_age_server:main',
             'client = service_example.get_age_client:main',
         ],
     },
