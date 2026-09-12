08d431dc994cf0b89be28a1ad391280accb5f9b4
Kristof Pasztor <pasztor.kristof.kp@gmail.com>
2026-09-12 14:02:21 +0200

    Check topic_example

--- VIIIAV55/topic_example/launch/topic.launch.py
+++ VIIIAV55/topic_example/launch/topic.launch.py
@@ -1,42 +1,46 @@
-from launch import LaunchDescription
 from launch_ros.actions import Node
+
+from launch import LaunchDescription
 from launch.actions import DeclareLaunchArgument
 from launch.substitutions import LaunchConfiguration
 
 
-
 def generate_launch_description():
     declare_to_greet = DeclareLaunchArgument(
-        'to_greet',
-        default_value='world',
-        description='Name of the person to greet'
+        "to_greet",
+        default_value="world",
+        description="Name of the person to greet",
     )
 
     declare_greeting = DeclareLaunchArgument(
-        'greeting',
-        default_value='Hello',
-        description='Greeting message'
+        "greeting",
+        default_value="Hello",
+        description="Greeting message",
     )
 
     publisher = Node(
-            package='topic_example',
-            executable='talker',
-            name='talker'
-        )
+        package="topic_example",
+        executable="talker",
+        name="talker",
+        parameters=[
+            {
+                "to_greet": LaunchConfiguration("to_greet"),
+                "greeting": LaunchConfiguration("greeting"),
+            }
+        ],
+    )
 
     subscriber = Node(
-                package='topic_example',
-                executable='listener',
-                name='listener',
-                parameters=[{
-                    'to_greet': LaunchConfiguration('to_greet'),
-                    'greeting': LaunchConfiguration('greeting')
-                }]
-            )
+        package="topic_example",
+        executable="listener",
+        name="listener",
+    )
 
-    return LaunchDescription([
-        declare_to_greet,
-        declare_greeting,
-        publisher,
-        subscriber,
-    ])
+    return LaunchDescription(
+        [
+            declare_to_greet,
+            declare_greeting,
+            publisher,
+            subscriber,
+        ]
+    )
